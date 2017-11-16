cc3ec85c4e7ce8deb8b2398e5b4a0f3c2ddc188a
bendera <szoftverhiba@gmail.com>
2017-11-16 01:37:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <header></header>
+    <main>
+      <img src="~assets/quasar-logo-full.svg" alt="Quasar PWA">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {}
+</script>
+
+<style lang="stylus">
+@import '~variables'
+
+main
+  text-align center
+  margin-top 40px
+
+header
+  margin 0
+  height 10px
+  background-color $primary
+</style>
