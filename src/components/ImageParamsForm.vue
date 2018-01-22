1d8bd7c88d213a9743b414b08bdb9832961eec64
bendera <szoftverhiba@gmail.com>
2018-01-22 02:04:19 +0100

    more polishing

--- src/components/ImageParamsForm.vue
+++ src/components/ImageParamsForm.vue
@@ -1,108 +1,139 @@
 <template>
   <div class="image-params-form">
-    <QCard>
-      <QCardTitle>Options</QCardTitle>
-      <QCardMain>
-        <QInput
-          type="number"
-          float-label="Width"
-          v-model="width"
-        />
-        <QInput
-          type="number"
-          float-label="Height"
-          v-model="height"
-        />
-        <QInput
-          type="text"
-          float-label="Text color"
-          v-model="textColor"
-        />
-        <QInput
-          type="text"
-          float-label="Background color"
-          v-model="backgroundColor"
-        />
-        <QBtn
-          icon="color_lens"
-          color="secondary"
-          @click="randomColors()"
-        >Random color</QBtn>
-        <QInput
-          type="text"
-          float-label="Caption"
-          v-model="caption"
-        />
-        <QInput
-          type="text"
-          float-label="Font family"
-          v-model="fontFamily"
-        />
-        <QInput
-          type="number"
-          float-label="Font size"
-          v-model="fontSize"
-        />
-        <QSelect
-          radio
-          float-label="Font weight"
-          v-model="fontWeight"
-          :options="[
-            {label: '100', value: '100'},
-            {label: '200', value: '200'},
-            {label: '300', value: '300'},
-            {label: '400', value: '400'},
-            {label: '500', value: '500'},
-            {label: '600', value: '600'},
-            {label: '700', value: '700'},
-            {label: '800', value: '800'},
-            {label: '900', value: '900'}
-          ]"
-        />
-        <QSelect
-          radio
-          float-label="Filetype"
-          v-model="filetype"
-          :options="[
-            {label: 'SVG', value: 'svg'},
-            {label: 'PNG', value: 'png'}
-          ]"
-        />
-      </QCardMain>
-    </QCard>
-    <QCard>
-      <QCardTitle>Output</QCardTitle>
-      <QCardMain>
-        <div id="canvas-demo"></div>
-        <div class="image-preview">
-          <img :src="dataURI" alt="preview image">
-        </div>
-        <QInput
-          stack-label="Data URI"
-          v-model="dataURI"
-          readonly
-          @focus="selectDataURI($event)"
-        />
-        <a
-        :href="dataURI"
-        class="q-btn row inline flex-center q-focusable q-hoverable relative-position q-btn-rectangle q-btn-big bg-primary text-white"
-        :download="friendlyFilename"
-        >
-          <div class="desktop-only q-focus-helper"></div>
-          <span class="q-btn-inner row col flex-center">Download image</span>
-        </a>
-      </QCardMain>
-    </QCard>
+    <QInput
+      type="number"
+      stack-label="Width"
+      v-model="width"
+    />
+    <QInput
+      type="number"
+      stack-label="Height"
+      v-model="height"
+    />
+    <QInput
+      type="text"
+      stack-label="Text color"
+      v-model="textColor"
+    />
+    <QInput
+      type="text"
+      stack-label="Background color"
+      v-model="backgroundColor"
+    />
+    <QBtn
+      :small="true"
+      icon="color_lens"
+      color="secondary"
+      @click="randomColors()"
+    >Random color</QBtn>
+    <QInput
+      type="text"
+      stack-label="Caption"
+      v-model="caption"
+    />
+    <QInput
+      type="text"
+      stack-label="Font family"
+      v-model="fontFamily"
+    >
+      <QAutocomplete
+        :debounce="0"
+        :static-data="{
+          field: 'value',
+          list: [
+            {label: 'Arial', value: 'Arial'},
+            {label: 'Arial Black', value: 'Arial Black'},
+            {label: 'Bookman', value: 'Bookman'},
+            {label: 'Comic Sans MS', value: 'Comic Sans MS'},
+            {label: 'Courier', value: 'Courier'},
+            {label: 'Courier New', value: 'Courier new'},
+            {label: 'Garamond', value: 'Garamond'},
+            {label: 'Georgia', value: 'Georgia'},
+            {label: 'Helvetica', value: 'Helvetica'},
+            {label: 'Impact', value: 'Impact'},
+            {label: 'Material Icons', value: 'Material Icons', sublabel: 'https://material.io/icons'},
+            {label: 'Open Sans', value: 'Open Sans'},
+            {label: 'Palatino', value: 'Palatino'},
+            {label: 'Times', value: 'Times'},
+            {label: 'Times New Roman', value: 'Times New Roman'},
+            {label: 'Trebuchet MS', value: 'Trebuchet MS'},
+            {label: 'Ubuntu', value: 'Ubuntu'},
+            {label: 'Verdana', value: 'Verdana'}
+          ]
+        }"
+      />
+    </QInput>
+    <QInput
+      type="number"
+      stack-label="Font size"
+      v-model="fontSize"
+    />
+    <QSelect
+      radio
+      stack-label="Font weight"
+      v-model="fontWeight"
+      :options="[
+        {label: '100', value: '100'},
+        {label: '200', value: '200'},
+        {label: '300', value: '300'},
+        {label: '400', value: '400'},
+        {label: '500', value: '500'},
+        {label: '600', value: '600'},
+        {label: '700', value: '700'},
+        {label: '800', value: '800'},
+        {label: '900', value: '900'}
+      ]"
+    />
+    <QSelect
+      radio
+      stack-label="Filetype"
+      v-model="filetype"
+      :options="[
+        {label: 'SVG', value: 'svg'},
+        {label: 'PNG', value: 'png'}
+      ]"
+    />
+    <div class="image-preview">
+      <img :src="dataURI" alt="preview image">
+    </div>
+    <QInput
+      stack-label="Data URI"
+      v-model="dataURI"
+      readonly
+      @focus="selectDataURI($event)"
+    />
+    <a
+    :href="dataURI"
+    class="
+      q-btn
+      row
+      inline
+      flex-center
+      q-focusable
+      q-hoverable
+      relative-position
+      q-btn-rectangle
+      q-btn-big
+      bg-primary
+      text-white
+      full-width
+      btn-download
+    "
+    :download="friendlyFilename"
+    >
+      <div class="desktop-only q-focus-helper"></div>
+      <span class="q-btn-inner row col flex-center">Download image</span>
+    </a>
   </div>
 </template>
 
 <script>
 import {
+  QAutocomplete,
   QBtn,
   QCard,
   QCardMain,
   QCardTitle,
-  QField,
   QInput,
   QOptionGroup,
   QSelect,
@@ -120,11 +151,11 @@ const renderers = {
 
 export default {
   components: {
+    QAutocomplete,
     QBtn,
     QCard,
     QCardMain,
     QCardTitle,
-    QField,
     QInput,
     QOptionGroup,
     QSelect
@@ -136,7 +167,7 @@ export default {
       textColor: '#ffffff',
       backgroundColor: '#cccccc',
       caption: '',
-      fontFamily: 'Helvetica, Arial, sans-serif',
+      fontFamily: 'Helvetica',
       fontSize: 40,
       fontWeight: '400',
       fontAlign: 'center',
@@ -215,6 +246,7 @@ export default {
 .image-params-form {
   margin: 0 auto;
   max-width: 90vw;
+  padding-bottom: 50px;
   width: 500px;
 }
 
@@ -229,11 +261,16 @@ export default {
   display: flex;
   justify-content: center;
   height: 200px;
+  margin-top: 50px;
 }
 
 .image-preview img {
   display: block;
   max-height: 100%;
   max-width: 100%;
+}
+
+.btn-download {
+  appearance: none;
 }
 </style>
